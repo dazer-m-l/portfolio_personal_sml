a08e02019c2426ac9874c660379926e2dd18bac2
dazer-m-l <dazer.mc1397@gmail.com>
2025-04-01 15:28:32 -0600

    Refactor: Cambios en vista mobile e ingresos url en front

--- frontend/script.js
+++ frontend/script.js
@@ -7,3 +7,27 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+function toggleMenu() {
+    document.querySelector(".nav__menu").classList.toggle("active");
+  }
+  
+  document.querySelector(".nav__toggle").addEventListener("click", toggleMenu);
+  
+  document.querySelectorAll(".nav__menu a").forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - 50,
+          behavior: "smooth"
+        });
+      }
+  
+      document.querySelector(".nav__menu").classList.remove("active");
+    });
+  });
+  
